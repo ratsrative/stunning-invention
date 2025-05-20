93876c9967d5fba64e7d58e4ef0d6e0cc426a2c9
ratsrative <rativerats@outlook.com>
2025-05-20 06:22:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import date
 
 import yfinance as yf
 from prophet import Prophet
-from prophet.plot import plot_plotly
+from prophet.plot import (plot, plot_components)
 from plotly import graph_objs as go
 
 START = "2015-01-01"
